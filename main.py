e51ed0ab6b8338b648f5bfe2337d5bfc3c5b8521
HenriqueLDSa <rique.ldsa@gmail.com>
2024-05-23 14:36:25 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyDO4PVZHAlcavT2n6mS9mCmuCAVrdswsHw")
+
+# Set up the model
+generation_config = {
+  "temperature": 0.5,
+}
+
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+]
+
+model = genai.GenerativeModel(model_name="gemini-1.0-pro",
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
+
+convo = model.start_chat(history=[])
+
+convo.send_message("Hello!")
+print(convo.last.text)
